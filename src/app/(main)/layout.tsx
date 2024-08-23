5b496ea1e1bb4c2db95fa0bab2a70e527c23196a
Zachary Gray <zac.gray@healthxchange.com>
2024-08-23 23:34:44 +0100

    feat(library): set up library context

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,20 @@
+'use client'
+
 import { Navbar } from '@/components/Navbar'
-import { ReactNode } from 'react'
+import { LibraryContext } from '@/context/LibraryContext'
+import { ReactNode, useState } from 'react'
 
 export default function MainLayout({ children }: { children: ReactNode }) {
+  const [libraryOpen, setLibraryOpen] = useState(false)
+
+  const toggleLibraryOpen = () => setLibraryOpen((prev) => !prev)
+
   return (
-    <>
+    <LibraryContext.Provider value={{ libraryOpen, toggleLibraryOpen }}>
       <header>
         <Navbar />
       </header>
       {children}
-    </>
+    </LibraryContext.Provider>
   )
 }
